4fd9646ea61ffc218cc1631c75ca834998f7c56e
Ahmad Sameh <41856063+AhmedSamehh@users.noreply.github.com>
2021-07-13 20:47:27 +0200

    handling home page view (vue)

--- fg_django/fg_django/settings.py
+++ fg_django/fg_django/settings.py
@@ -33,9 +33,14 @@ INSTALLED_APPS = [
     'product',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8081",
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
