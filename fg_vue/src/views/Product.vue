e850117de808225bb8ea58fbbfbd4e63049e0aac
Ahmad Sameh <41856063+AhmedSamehh@users.noreply.github.com>
2021-07-15 18:39:59 +0200

    add to cart toast

--- fg_vue/src/views/Product.vue
+++ fg_vue/src/views/Product.vue
@@ -26,6 +26,7 @@
 
 <script>
 import axios from 'axios'
+import {toast} from 'bulma-toast'
 export default {
     name:'Product',
     data(){
@@ -58,6 +59,15 @@ export default {
                 quantity: this.quantity
             }
             this.$store.commit('addToCart', item)
+
+            toast({
+                message: 'The product was added to cart',
+                type: 'is-success',
+                dismissible: true,
+                pauseOnHover: true,
+                duration: 2000,
+                position: 'top-center'
+            })
         }
     }
 }
