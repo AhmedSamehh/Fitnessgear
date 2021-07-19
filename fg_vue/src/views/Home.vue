018633922891fabc6b65e131050b4f17e6ea0a59
Ahmad Sameh <41856063+AhmedSamehh@users.noreply.github.com>
2021-07-19 17:39:40 +0200

    created product details page

--- fg_vue/src/views/Home.vue
+++ fg_vue/src/views/Home.vue
@@ -14,22 +14,14 @@
       <div class="column is-12">
         <h2 class="is-size-2 has-text-centered">Latest products</h2>
       </div>
-      <div class="column is-3" v-for="product in latestProducts" :key="product.id">
-        <div class="box">
-          <figure class="image m-4">
-            <img :src="product.get_thumbnail">
-          </figure>
-          <h3 class="is-size-4">{{product.name}}</h3>
-          <p class="is-size-6 has-text-success">{{product.price}}</p>
-          <router-link :to="product.get_absolute_url" class="button is-dark mt-4"><strong>View details</strong></router-link>
-        </div>
-      </div>
+      <ProductBox v-for="product in latestProducts" :key="product.id" :product="product" />
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import ProductBox from '@/components/ProductBox'
 export default {
   name: 'Home',
   data(){
@@ -38,6 +30,7 @@ export default {
     }
   },
   components: {
+    ProductBox
   },
   mounted(){
     this.getLatestProducts()
@@ -59,10 +52,3 @@ export default {
 }
 </script>
 
-<style scoped>
-  .image {
-    margin-top: -1.25rem;
-    margin-left: -1.25rem;
-    margin-right: -1.25rem;
-  }
-</style>
