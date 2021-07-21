c4f2240f3260406460cf1c0d91755b389a105f3c
Ahmad Sameh <41856063+AhmedSamehh@users.noreply.github.com>
2021-07-21 15:19:45 +0200

    search functionality

--- fg_vue/src/App.vue
+++ fg_vue/src/App.vue
@@ -10,6 +10,25 @@
         </a>
       </div>
       <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <form action="/search" method="get">
+              <div class="field has-addons">
+                <div class="control">
+                  <input type="text" placeholder="What are you looking for?" name="query" class="input">
+                </div>
+                <div class="control">
+                  <button class="button is-success">
+                    <span class="icon">
+                      <i class="fas fa-search"></i>
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
         <div class="navbar-end">
           <router-link to="/summer" class="navbar-item">Summer</router-link>
           <router-link to="/winter" class="navbar-item">Winter</router-link>
